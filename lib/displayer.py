5c0d06d5da62f70b20dc3fff31e308e19d1e413d
Riley White <rileywhite89@gmail.com>
2023-04-17 18:26:52 -0400

    massivly speed up displayer

--- lib/displayer.py
+++ lib/displayer.py
@@ -14,24 +14,24 @@ class displayer:
         clock = pygame.time.Clock()
         clock.tick(60)
 
+        self._map_shape = _map_shape
         self.has_gap = has_gap
         self.pix_size = pix_size
         self.screen = pygame.display.set_mode((_map_shape[1]*self.pix_size,
                                                _map_shape[0]*self.pix_size))
+        blank = np.zeros((*_map_shape, 3), dtype=np.uint8)
+        # blank = blank.repeat(self.pix_size, axis=0).repeat(self.pix_size, axis=1)
+        self.image = pygame.surfarray.make_surface(blank)
+        self.scaled_image = pygame.transform.scale(self.image, (_map_shape[1]*self.pix_size,
+                                            _map_shape[0]*self.pix_size))
 
     def update(self, _map):
         self.screen.fill((255,255,255))
-
-        for i in range(_map.shape[0]):
-            for j in range(_map.shape[1]):
-                x = j * self.pix_size + int(self.pix_size/2)
-                y = i * self.pix_size + int(self.pix_size/2)
-                if self.has_gap:
-                    size = min(int(self.pix_size * 0.75), self.pix_size-2)
-                else:
-                    size = self.pix_size
-                s = pygame.Surface((size,size))
-                c = (_map[i,j]*256).astype(int)[:3]
-                s.fill(c)
-                self.screen.blit(s, (x-int(size/2), y-int(size/2)))
+        c = (_map*256).astype(np.uint8)[:, :, :3]
+        c = np.transpose(c, (1, 0, 2))
+        pygame.surfarray.blit_array(self.image, c)
+        self.scaled_image = pygame.transform.scale(self.image, (self._map_shape[1]*self.pix_size,
+                                            self._map_shape[0]*self.pix_size),
+                                            self.scaled_image)
+        self.screen.blit(self.scaled_image, (20,20))
         pygame.display.update()
